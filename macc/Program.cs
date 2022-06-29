ae23805e6e91a971631bd676d1efb44841015f76
Valentin <vellshow@bk.ru>
2022-06-29 22:08:20 +0300

    задача6

---
+++ macc/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int[] array = { 5, 4, 7, 73, 32, 8, 76, 14, 14 };
+
+int n = array.Length;
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int find = b;
+
+int index = 0;
+
+while(index < n)
+{  
+    if(array[index] == find)
+    {
+         Console.WriteLine(index);
+         break;        
+    }
+    index++;
+}
---
+++ test_06/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+
+while (b <= a)
+{
+    Console.WriteLine(b);
+    b =( b + 2);
+}
