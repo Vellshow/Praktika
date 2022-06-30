5136205ce0aab8200e50b19b0d50e93df791a397
Valentin <vellshow@bk.ru>
2022-06-30 16:18:07 +0300

    задача12

---
+++ test_12/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Ведите число которое хотите проверить:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+string result = num1%num2 == 0? $"{num1} Кратное {num2}" : $"{num1} Остаток {num2} = {num1%num2}";
+Console.WriteLine(result);
