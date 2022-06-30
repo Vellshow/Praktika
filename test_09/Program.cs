91f57ff9700caa46e3b0f50785f3cc170d76de27
Valentin <vellshow@bk.ru>
2022-06-30 11:47:46 +0300

    задача9

---
+++ test_09/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int random =new Random().Next(10, 100);
+Console.WriteLine($"Рандомное число {random}");
+
+int first = random / 10;
+int second = random % 10;
+
+if(first > second) Console.WriteLine($"Первое число больше: {first}");
+else if(first < second) Console.WriteLine($"Второе число больше: {second}");
+else Console.WriteLine($"Числа равны: {random}");
