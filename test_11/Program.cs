79cf941e6e686de2f9d908d8ed6c0e9e1bfaf01e
Valentin <vellshow@bk.ru>
2022-06-30 12:30:53 +0300

    задача11

---
+++ test_11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int random = new Random().Next(100, 1000);
+Console.WriteLine($"Рандомное число от 100 до 1000 => {random}");
+
+int a = random / 100;
+int b = (random / 10) % 10;
+int c = random % 10;
+
+Console.WriteLine(a);
+Console.WriteLine(c);
