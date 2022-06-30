6f4fa0e81b46c7c42b0cbcdbb55a77825af4f537
Valentin <vellshow@bk.ru>
2022-06-30 17:28:00 +0300

    задача14

---
+++ test_14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+
+if(n % num1 == 0 && n % num2 == 0)
+{
+    Console.WriteLine("Кратное");
+}
+else
+{
+    Console.WriteLine("Некратное");
+}
