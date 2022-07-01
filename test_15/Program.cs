e4ab28a9eb118072e5a6bc4bb54bb03f690e9a7b
Valentin <vellshow@bk.ru>
2022-07-01 14:34:54 +0300

    задача15

---
+++ test_15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите цифру дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num == 6 || num == 7) Console.WriteLine ("Да выходной");
+else if(num < 6) Console.WriteLine("Нет не выходной");
+else Console.WriteLine($"Данного дня недели не существует {num}");
