44354bff042b6b4fe576a30aeb8ce92aa5920919
Valentin <vellshow@bk.ru>
2022-07-01 15:53:35 +0300

    задача10

---
+++ test_10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+var rnd = new Random();
+int num = rnd.Next(100, 1000);
+Console.WriteLine(num);
+int a1 = 0;
+
+if (num < 1000)
+{
+    a1 = (num / 10) % 10;
+    Console.Write(a1);
+}
