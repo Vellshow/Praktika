ade0abbdf95fafa66f32037723eeffffda4519e4
Valentin <vellshow@bk.ru>
2022-07-01 15:36:23 +0300

    задача13

---
+++ test_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+var rnd = new Random();
+int a = rnd.Next(10, 1000);
+int num1 = 0;
+
+Console.WriteLine(a);
+
+if(a<100) Console.WriteLine("Нету третий цифры");
+else if(a>10)
+{
+    num1 = (a%10);
+    Console.WriteLine($"Третья цифра:{num1}");
+}
