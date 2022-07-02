1421971e1f86ab2f00f75c01a75417a06c80c378
Valentin <vellshow@bk.ru>
2022-07-02 18:41:30 +0300

    тест_задание

---
+++ per_test/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Qur(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Не корректный ввод";
+}
+
+string result = Qur(x, y);
+Console.WriteLine(result);
