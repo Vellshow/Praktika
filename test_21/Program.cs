55bcadccf701f9b7a11c13ed254f4e67e8d5b292
Valentin <vellshow@bk.ru>
2022-07-02 22:01:53 +0300

    задача21

---
+++ test_21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите значение первой Х: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение первой Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение второго Х: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение второго Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+double sum = Math.Sqrt(Math.Pow((x2 -x1),2) + Math.Pow((y2 -y1), 2));
+Console.WriteLine(Math.Round(sum, 3));
