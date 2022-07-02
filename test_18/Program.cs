0944b2bf629c14cc72a85aebf2a7503dac954b6c
Valentin <vellshow@bk.ru>
2022-07-02 20:26:27 +0300

    задача18

---
+++ test_18/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Первая четверть => 1 ");
+Console.WriteLine("Вторая четверть => 2 ");
+Console.WriteLine("Третья четверть => 3 ");
+Console.WriteLine("Четвертая четверть => 4 ");
+Console.Write("Введите число соответствующий четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1: Console.WriteLine("x > 0, y > 0"); break;
+    case 2: Console.WriteLine("x < 0, y > 0"); break;
+    case 3: Console.WriteLine("x < 0, y < 0"); break;
+    case 4: Console.WriteLine("x > 0, y < 0"); break;
+    default: Console.WriteLine("Данной четверти нет"); break;
+}
