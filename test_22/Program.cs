83b9f5c0cd426f76b45682d310a9466917402b6a
Valentin <vellshow@bk.ru>
2022-07-03 20:27:13 +0300

    задача22

---
+++ test_22/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+while (a <= n)
+{
+    double b = Math.Pow(a, 2);
+    Console.WriteLine($"{a}  |  {b}");
+    a +=1;
+}
+
+
+
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int a = 1;
+// while (a <= n)
+// {
+//     Console.WriteLine($"{a}  |  {a*a}");
+//     a++;
+// }
