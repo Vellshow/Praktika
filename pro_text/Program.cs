1c03f6f321d666c85bbae1401b00db33d14bcf78
Valentin <vellshow@bk.ru>
2022-07-05 22:07:21 +0300

    pro

---
+++ pro_text/Program.cs
@@ -0,0 +1,33 @@
+﻿// void Metod2(string mse)
+// {
+//     Console.WriteLine(mse);
+// }
+
+// Metod2("Валентин");
+
+// void ms(string sm, int count)
+// {
+//     int i = 0;
+//     while(i< count)
+//     {
+//         Console.WriteLine(sm);
+//         i++;
+//     }
+// }
+// ms(sm: "Валентин", 5);
+
+Console.WriteLine("");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Ber(int number)
+{
+    int i = 0;
+    for ( ; number > 0; i++)
+    {
+        number = number / 10;
+        
+    }
+    return i;
+}
+int re = Ber(num);
+Console.WriteLine(re);
