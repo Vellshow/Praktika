ff2c25f6a6746531e84edc73467ec62d1bac5161
Valentin <vellshow@bk.ru>
2022-07-06 09:42:23 +0300

    mac

---
+++ mac/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] array = { 1, 2, 5, 4, 7, 69, 55, 4 };
+
+void Printarray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+Printarray(array);
+Console.WriteLine();
+
+void SelectionSort(int[] arri)
+{
+    for (int i = 0; i < arri.Length - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < arri.Length; j++)
+        {
+            {
+            if (arri[j] < arri[min]) min = j;
+            }
+        }
+        int temp = arri[i];
+        arri[i] = arri[min];
+        arri[min] = temp;
+    }
+}
+
+SelectionSort(array);
+Printarray(array);
