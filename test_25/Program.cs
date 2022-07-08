9a592083c88db95d7d7b33babe9edb72178af5e0
Valentin <vellshow@bk.ru>
2022-07-08 23:36:44 +0300

    задачи

---
+++ test_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number =Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 0; i < number; i++)
+{
+    sum = sum + number[i];    
+}
+Console.Write(sum);
---
+++ test_31/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int [] array = new int [12];
+var rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i] = rnd.Next(-10, 20)} ");
+}
+Console.WriteLine();
+
+int poll = 0;
+int otr = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]> 0)
+    {
+        poll = poll +array[i];
+    }
+    if(array[i]< 0)
+    {
+        otr = otr + array[i];
+    }
+}
+Console.WriteLine(poll);
+Console.Write(otr);
